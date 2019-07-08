f5754f2988fde0fe6d647e2ffb439b9553250225
Dinakaran Santhanam <dinakaran.santhanam@vivriticapital.com>
2019-07-08 18:40:52 +0530

    Calculator ui added

---
+++ packages/calculator-ui/src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Addition = ({add}) => {
+    const aElm = React.createRef();
+    const bElm = React.createRef();
+    const [ a, setA ] = useState(0)
+    const [ b, setB ] = useState(0)
+    return (
+        <div style={{width: '320px', display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+            <input
+                type="text"
+                onChange={() => setA(aElm.current.value)} />
+            <input
+                type="text"
+                onChange={() => setB(bElm.current.value)} />
+
+            <button onClick={() => add(a, b)}>Add</button>
+        </div>
+    );
+}
+
+export default Addition;
